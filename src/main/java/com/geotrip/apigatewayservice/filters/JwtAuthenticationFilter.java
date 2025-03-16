859f8aecdf675af12a466a52f76beeabeda9db98
Sayantika Ghosh <sayantika.ghosh@insideiim.com>
2025-03-17 02:55:52 +0530

    Update: new public endpoint for driver client test

--- src/main/java/com/geotrip/apigatewayservice/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/geotrip/apigatewayservice/filters/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import com.geotrip.apigatewayservice.services.JwtService;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -31,8 +32,10 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             ServerHttpRequest request = exchange.getRequest();
 
             System.out.println("uri "+request.getURI());
+
             String path = request.getURI().getPath();
 
+            System.out.println("path "+path);
 
             //skip auth for these endpoints
             if(isPublicRoute(path)) {
@@ -41,21 +44,38 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
             String authorization = request.getHeaders().getFirst("Authorization");
 
+            String token = null;
             if(authorization == null || !authorization.startsWith("Bearer ")) {
-                throw new Exception();
+                HttpCookie httpCookie = request.getCookies().getFirst("authToken");
+                if(httpCookie != null) {
+                    System.out.println("cookie set"+httpCookie.getValue());
+                    token = httpCookie.getValue();
+                }
+            }
+            else{
+                System.out.println("bearer token"+authorization);
+                token = authorization.substring(7);
             }
 
-            String token = authorization.substring(7);
+            System.out.println("token "+token);
+
+            if(token == null) {
+                throw new Exception("No valid JWT token found in header or cookie");
+            }
+
+            System.out.println("Auth Token: "+token);
 
             if(jwtService.isTokenExpired(token)) {
                 throw new Exception();
             }
 
+            System.out.println("Auth Token Not Expired: "+token);
+
             String email = jwtService.extractEmail(token);
             String role = jwtService.extractRole(token);
             ServerHttpRequest modifiedRequest = request.mutate()
                     .header("X-User-Email", email)
-                    .header("X-User-Role", "ROLE_"+role)
+                    .header("X-User-Role", role)
                     .build();
             return chain.filter(exchange.mutate().request(modifiedRequest).build());
 
@@ -67,6 +87,6 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
     }
 
     private boolean isPublicRoute(String path) {
-        return path.matches("^/api/v1/auth/register/(passenger|driver)$") || path.equals("/api/v1/auth/login");
+        return path.matches("^/api/v1/auth/register/(passenger|driver)$") || path.equals("/api/v1/auth/login") || path.equals("/api/v1/auth/websocket");
     }
 }
